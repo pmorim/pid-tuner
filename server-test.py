15018445ce8895bdc4b99671b30cb7e82dc421e2
Pedro Morim <1180798@isep.ipp.pt>
2021-05-16 00:19:12 +0100

    Added .env to server

--- server-test.py
+++ server-test.py
@@ -2,24 +2,28 @@ from flask import Flask
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS, cross_origin
 
+import os
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
-app.config['SECRET_KEY'] = 'secret!'
+app.config['SECRET_KEY'] = os.environ.get('SECRET')
 socketio = SocketIO(app)
 CORS(app)
 
 @socketio.on('connect')
 @cross_origin()
-def test_connect():
-    emit('server-client', 'O server diz que foi ao Brasil buscar ouro')
+def handle_connection():
+  emit('server-client', 'O server diz que foi ao Brasil buscar ouro')
 
 
 @socketio.on('client-server')
 @cross_origin()
-def test_connect(msg):
-    print("\n" + str(msg))
+def handle_client_msg(msg):
+  print("\n" + str(msg))
 
 
 if __name__ == '__main__':
-    app.run(host="localhost", port=5000)
-    socketio.run(app)
+  app.run(host="localhost", port=os.environ.get('PORT'))
+  socketio.run(app)
