365a4306c53e0fa17b88380b40a451fddb968fbc
Pedro Morim <1180798@isep.ipp.pt>
2021-06-04 19:57:11 +0100

    Removed useless imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { io } from 'socket.io-client';
 
 // Custom components
--- client/src/components/Steps/System.jsx
+++ client/src/components/Steps/System.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { io } from 'socket.io-client';
 import MathJax from 'react-mathjax-preview';
 
 // Custom components
