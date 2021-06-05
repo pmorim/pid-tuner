e2a35284f8775031404163a99b987443f469176a
RuiSargo <ruiduartesargo@gmail.com>
2021-06-05 02:20:22 +0100

    Mudei um pouco, faltam as contas dos metodos
    Vejam se gostam da forma como explico o formato e tipo das variáveis dos "json".
    Assumo que na GUI já se vai impedir o erro de se escolher um controlo "incompatível"  com um determinado método (pelo menos segundo os pdfs das teóricas)

--- server_funcs.py
+++ server_funcs.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def system_model_func(sys_model):
+def model_func(sys_model):
   """Calculate the points of the system model.
 
   Args:
@@ -12,30 +12,49 @@ def system_model_func(sys_model):
     dict of float: The x and y coordinates of the points.
   """
 
-  k = sys_model["k"]
-  tau = sys_model["tau"]
-  tauD = sys_model["tauD"]
-  t_max = sys_model["t_max"] or 50
-  res = sys_model["res"] or 0.5
-  A = sys_model["A"] or 1
+  k = float(sys_model["k"])
+  tau = float(sys_model["tau"])
+  tauD = float(sys_model["tauD"])
+
+  t_max = float(sys_model.get("t_max")) or 5*tau + tauD
+  res = float(sys_model.get("res")) or 0.5
+  A = float(sys_model.get("A")) or 1
+  
 
   size = int(t_max/res)
   print(size)
 
+# only appending to data values >= 0, format = [{"x": x, "y", y}, ... ]
   data = []
   for t in range(size):
-    data.append({"x": t*res, "y": k*A*(1-np.e**(-(t-tauD)/tau))})
+    y = k*A*(1-np.e**(-(t-tauD)/tau))
+    if y < 0:
+        data.append({"x": t*res, "y": 0})
+    else:
+        data.append({"x": t*res, "y": y})
 
   return data
     
 
-def controller_type_func(contr_type):
+def control_func(control):
+  """
+  #k = float(control["k"])
+  #tau = float(control["tau"])
+  #tauD = float(control["tauD"])
+  #type = type["method"]
+  method = control["method"]
+
+  if method == "ZN":
+    ziegler_nichols_func(control)
+    pass
+  if method == "CC":
+    cohen_coon_func(control)
+    pass
+  if method == "IMC":
+    imc_func(control)
+    pass
+  """
+  #return k_gains # format = {"kp": %f, "ki": %f, "kd", %f}
   pass
 
 
-def sintony_method_func(synt_met):
-  pass
-
-
-def k_consts_func(pid_consts):
-  pass
