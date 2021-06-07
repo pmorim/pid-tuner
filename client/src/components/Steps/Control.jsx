c9159cc1a8a4c48fa82ac07f7827b0605ca1e1ca
Pedro Morim <1180798@isep.ipp.pt>
2021-06-07 17:10:11 +0100

    Added Control Page

--- client/src/components/Steps/Control.jsx
+++ client/src/components/Steps/Control.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MathJax from 'react-mathjax-preview';
+import { controlTypes } from './data/control';
 
 // Custom components
 import { Step, StepBody, StepDesc, StepNav, StepTitle } from '../Stepper';
 import { NextBtn, BackBtn } from '../Buttons';
 
 // Chakra-UI components
-import { Text } from '@chakra-ui/layout';
+import { HStack, Text } from '@chakra-ui/layout';
+import {
+  FormControl,
+  FormHelperText,
+  FormLabel,
+} from '@chakra-ui/form-control';
+import { Radio, RadioGroup } from '@chakra-ui/radio';
+import {
+  Table,
+  TableCaption,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from '@chakra-ui/table';
 
 export const ControlStep = ({ socket, ...rest }) => {
+  const [control, setControl] = useState('PID');
+
   return (
     <Step {...rest}>
       <StepTitle>Control type</StepTitle>
@@ -25,7 +43,50 @@ export const ControlStep = ({ socket, ...rest }) => {
         />
       </StepDesc>
 
-      <StepBody></StepBody>
+      <StepBody>
+        <FormControl>
+          <FormLabel>Choose the type of control</FormLabel>
+          <RadioGroup value={control} onChange={setControl}>
+            <HStack spacing={5}>
+              {controlTypes.map(type => (
+                <Radio
+                  key={type.name}
+                  colorScheme="teal"
+                  size="lg"
+                  value={type.name}
+                >
+                  {type.name}
+                </Radio>
+              ))}
+            </HStack>
+          </RadioGroup>
+          <FormHelperText>
+            If you are unsure which one to pick, we recommend: PID.
+          </FormHelperText>
+        </FormControl>
+
+        {controlTypes
+          .filter(type => type.name === control)
+          .map(type => (
+            <Table key={type.name} variant="simple">
+              <TableCaption>Pros and Cons of {type.name} control</TableCaption>
+              <Thead>
+                <Tr>
+                  <Th>Pros</Th>
+                  <Th>Cons</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {type.info.map((row, i) => (
+                  <Tr key={i}>
+                    <Td>{row.pro ?? '-'}</Td>
+                    <Td>{row.con ?? '-'}</Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          ))}
+      </StepBody>
 
       <StepNav>
         <BackBtn />
---
+++ client/src/components/Steps/data/control.js
@@ -0,0 +1,45 @@
+export const controlTypes = [
+  {
+    name: 'P',
+    info: [
+      {
+        pro: '',
+        con: '',
+      },
+    ],
+  },
+  {
+    name: 'PI',
+    info: [
+      {
+        pro: '',
+        con: '',
+      },
+    ],
+  },
+  {
+    name: 'PD',
+    info: [
+      {
+        pro: '',
+        con: '',
+      },
+    ],
+  },
+  {
+    name: 'PID',
+    info: [
+      {
+        pro: "It's Cool",
+        con: 'Hard to implement',
+      },
+      {
+        pro: "Doesn't suck",
+        con: 'Fuck integrals',
+      },
+      {
+        con: 'Fuck derivatives',
+      },
+    ],
+  },
+];
